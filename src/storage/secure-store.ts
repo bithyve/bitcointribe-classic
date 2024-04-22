1124c8cc14504897dd569c2ac737dcb04696ffbf
akshay8551 <akshaychavan8551@gmail.com>
2024-04-22 17:41:37 +0530

    Added verifyBiometricAuth method

--- src/storage/secure-store.ts
+++ src/storage/secure-store.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { Platform } from 'react-native'
-import * as SecureStore from 'react-native-encrypted-storage'
-import RNSecureStorage, { ACCESSIBLE } from 'rn-secure-storage'
-import config from '../bitcoin/HexaConfig'
+import NodeRSA from 'node-rsa';
+import { Platform } from 'react-native';
+import * as SecureStore from 'react-native-encrypted-storage';
+import * as Keychain from 'react-native-keychain';
+import RNSecureStorage, { ACCESSIBLE } from 'rn-secure-storage';
+import config from '../bitcoin/HexaConfig';
 
 export const store = async ( hash, enc_key ) => {
   try {
@@ -83,3 +85,51 @@ export const remove = async ( key ) => {
   }
   return true
 }
+
+// Biometrics
+export const storeBiometricPubKey = async (pubKey: string) => {
+  try {
+    const credentials = await Keychain.getGenericPassword();
+    const password = credentials.password;
+    const reset = await Keychain.resetGenericPassword();
+    if (reset) {
+      const pass = {
+        ...password,
+        pubKey,
+      };
+      await Keychain.setGenericPassword(config.ENC_KEY_STORAGE_IDENTIFIER, JSON.stringify(pass), {
+        accessible: Keychain.ACCESSIBLE.WHEN_UNLOCKED,
+      });
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+export const verifyBiometricAuth = async (signature: string, payload: string) => {
+  try {
+    const keychain = await Keychain.getGenericPassword();
+    const credentials = JSON.parse(keychain.password);
+    const publicKeyBuffer = Buffer.from(credentials.pubKey, 'base64');
+    const key = new NodeRSA();
+    const signer = key.importKey(publicKeyBuffer, 'public-der');
+    const isVerified = signer.verify(Buffer.from(payload), signature, 'utf8', 'base64');
+    if (isVerified) {
+      return {
+        success: true,
+        encryptedKey: credentials.enc_key,
+        hash: credentials.hash,
+      };
+    }
+    return {
+      success: false,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+    };
+  }
+};
