2dab55a44e63adb1b0f0f42fd9e5fcd3a7969c31
akshay8551 <akshaychavan8551@gmail.com>
2024-04-24 13:23:09 +0530

    Added login method's sagas and actions

--- src/store/actions/storage.ts
+++ src/store/actions/storage.ts
@@ -1,9 +1,12 @@
+import LoginMethod from 'src/common/interfaces/LoginMethod'
 import { Wallet } from '../../bitcoin/utilities/Interface'
 
 // types and action creators (saga): dispatched by saga workers
 export const UPDATE_WALLET = 'UPDATE_WALLET'
 export const UPDATE_USER_NAME = 'UPDATE_USER_NAME'
 export const KEY_FETCHED = 'KEY_FETCHED'
+export const CHANGE_LOGIN_METHOD = 'CHANGE_LOGIN_METHOD'
+export const SET_LOGIN_METHOD = 'SET_LOGIN_METHOD'
 
 export const updateWallet = ( wallet: Wallet ) => {
   return {
@@ -30,3 +33,19 @@ export const keyFetched = ( key ) => {
     }
   }
 }
+
+// Biometrics method
+export const changeLoginMethod = (method: LoginMethod, pubKey: string = '') => ({
+  type: CHANGE_LOGIN_METHOD,
+  payload: {
+    method,
+    pubKey,
+  },
+});
+
+export const setLoginMethod = (method: LoginMethod) => ({
+  type: SET_LOGIN_METHOD,
+  payload: {
+    method,
+  },
+});
