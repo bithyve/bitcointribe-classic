2a474163f0bce5d44705e2fa70bd220ad1fd2b4d
akshay8551 <akshaychavan8551@gmail.com>
2024-05-03 12:10:27 +0530

    removed logs

--- src/storage/secure-store.ts
+++ src/storage/secure-store.ts
@@ -10,7 +10,6 @@ export const store = async (hash, enc_key) => {
   try {
     if (Platform.OS === 'android') {
       if (await RNSecureStorage.exists(config.ENC_KEY_STORAGE_IDENTIFIER)) {
-        console.log('Old key identified, removing...');
         await RNSecureStorage.remove(config.ENC_KEY_STORAGE_IDENTIFIER);
       }
 
@@ -25,7 +24,6 @@ export const store = async (hash, enc_key) => {
         }
       );
     } else {
-      console.log('store else - ', hash, enc_key);
       await SecureStore.default.setItem(
         config.ENC_KEY_STORAGE_IDENTIFIER,
         JSON.stringify({
@@ -52,7 +50,6 @@ export const fetch = async (hash_current) => {
     } else {
       value = await SecureStore.default.getItem(config.ENC_KEY_STORAGE_IDENTIFIER);
     }
-    console.log('value: ', value, '\n hash_current: ', hash_current);
     if (value) {
       ({ hash, enc_key } = JSON.parse(value));
 
@@ -70,7 +67,6 @@ export const fetch = async (hash_current) => {
 
 export const remove = async (key) => {
   try {
-    console.log('trying to remove');
     if (Platform.OS === 'android') {
       if (await RNSecureStorage.exists(key)) {
         await RNSecureStorage.remove(key);
@@ -97,27 +93,23 @@ export const storeBiometricPubKey = async (pubKey: string) => {
       if (await RNSecureStorage.exists(config.ENC_KEY_STORAGE_IDENTIFIER)) {
         value = await RNSecureStorage.get(config.ENC_KEY_STORAGE_IDENTIFIER);
       }
-      console.log('value', value);
       const { hash, enc_key } = JSON.parse(value);
       pass = {
         hash,
         enc_key,
         pubKey,
       };
-      console.log('storeBiometricPubKey android pass', pass);
       await RNSecureStorage.set(config.ENC_KEY_STORAGE_IDENTIFIER, JSON.stringify(pass), {
         accessible: ACCESSIBLE.WHEN_UNLOCKED,
       });
     } else {
       value = await SecureStore.default.getItem(config.ENC_KEY_STORAGE_IDENTIFIER);
-      console.log('else store bio value', value);
       const { hash, enc_key } = JSON.parse(value);
       pass = {
         hash,
         enc_key,
         pubKey,
       };
-      console.log('secure store - else', pass);
       await SecureStore.default.setItem(config.ENC_KEY_STORAGE_IDENTIFIER, JSON.stringify(pass));
     }
   } catch (err) {
@@ -137,13 +129,10 @@ export const verifyBiometricAuth = async (signature: string, payload: string) =>
       value = await SecureStore.default.getItem(config.ENC_KEY_STORAGE_IDENTIFIER);
     }
     const { pubKey, hash, enc_key } = JSON.parse(value);
-    console.log('secure store hash', hash, '\n enc_key - ' + enc_key);
-    console.log('secure store pubKey', pubKey);
     const publicKeyBuffer = Buffer.from(pubKey, 'base64');
     const key = new NodeRSA();
     const signer = key.importKey(publicKeyBuffer, 'public-der');
     const isVerified = signer.verify(Buffer.from(payload), signature, 'utf8', 'base64');
-    console.log('isVerified', isVerified);
     if (isVerified) {
       return {
         success: true,
