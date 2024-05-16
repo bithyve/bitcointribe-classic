f7bf3d2e6934a11954dcd6dcb37ae51fb50b730f
akshay8551 <akshaychavan8551@gmail.com>
2024-05-16 11:51:11 +0530

    removed unused code

--- src/pages/RestoreHexaWithKeeper/RestoreSeedWordsContent.tsx
+++ src/pages/RestoreHexaWithKeeper/RestoreSeedWordsContent.tsx
@@ -180,21 +180,6 @@ const RestoreSeedWordsContent = (props) => {
         backgroundColor: Colors.backgroundColor,
       }}
     >
-      {/* {
-        showLoader &&
-        <View style={{
-          position: 'absolute',
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0,
-          alignItems: 'center',
-          justifyContent: 'center',
-          zIndex: 10
-        }}>
-          <ActivityIndicator size="large" color={Colors.babyGray} />
-        </View>
-      } */}
       <SafeAreaView
         style={{
           flex: 0,
