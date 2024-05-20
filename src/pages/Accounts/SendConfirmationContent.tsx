4ccd53b47d543c1fb572bd10e9f9419092200fd2
akshay8551 <akshaychavan8551@gmail.com>
2024-05-20 10:05:27 +0530

    SendConfirmation - removed unused styles

--- src/pages/Accounts/SendConfirmationContent.tsx
+++ src/pages/Accounts/SendConfirmationContent.tsx
@@ -10,11 +10,11 @@ import LinearGradient from 'react-native-linear-gradient'
 import { RFValue } from 'react-native-responsive-fontsize'
 import {
   heightPercentageToDP as hp,
-  widthPercentageToDP as wp,
+  widthPercentageToDP as wp
 } from 'react-native-responsive-screen'
 import Colors from '../../common/Colors'
-import Fonts from '../../common/Fonts'
 import { RecipientDescribing } from '../../common/data/models/interfaces/RecipientDescribing'
+import Fonts from '../../common/Fonts'
 import { AppBottomSheetTouchableWrapper } from '../../components/AppBottomSheetTouchableWrapper'
 import RecipientComponent from './RecipientComponent'
 
@@ -143,15 +143,15 @@ export default function SendConfirmationContent( props ) {
 }
 
 const styles = StyleSheet.create( {
-  box: {
-    backgroundColor: Colors.backgroundColor1,
-    marginRight: wp( '5%' ),
-    marginLeft: wp( '5%' ),
-    marginBottom: hp( '3%' ),
-    borderRadius: 10,
-    justifyContent: 'center',
-    padding: wp( '5%' ),
-  },
+  // box: {
+  //   backgroundColor: Colors.backgroundColor1,
+  //   marginRight: wp( '5%' ),
+  //   marginLeft: wp( '5%' ),
+  //   marginBottom: hp( '3%' ),
+  //   borderRadius: 10,
+  //   justifyContent: 'center',
+  //   padding: wp( '5%' ),
+  // },
   successModalHeaderView: {
     marginBottom: hp( '1%' ),
     marginTop: hp( '1%' ),
@@ -166,54 +166,48 @@ const styles = StyleSheet.create( {
     fontSize: RFValue( 11 ),
     fontFamily: Fonts.Regular,
   },
-  successModalAmountView: {
-    marginRight: wp( '10%' ),
-    marginLeft: wp( '10%' ),
-  },
-  successModalWalletNameText: {
-    color: Colors.black,
-    fontSize: RFValue( 25 ),
-    fontFamily: Fonts.Regular,
-    textAlign: 'center',
-    paddingRight: 10,
-    flex: 1,
-  },
-  successModalAmountImage: {
-    width: wp( '15%' ),
-    height: wp( '15%' ),
-    marginRight: 15,
-    marginLeft: 10,
-    // marginBottom: wp("1%"),
-    resizeMode: 'contain',
-  },
-  successModalAmountText: {
-    color: Colors.black,
-    fontFamily: Fonts.Regular,
-    fontSize: RFValue( 21 ),
-    marginLeft: 5,
-  },
-  successModalAmountUnitText: {
-    color: Colors.borderColor,
-    fontFamily: Fonts.Regular,
-    fontSize: RFValue( 11 ),
-  },
-  successModalAmountInfoView: {
-    flex: 0.4,
-    marginRight: wp( '10%' ),
-    marginLeft: wp( '10%' ),
-  },
+  // successModalAmountView: {
+  //   marginRight: wp( '10%' ),
+  //   marginLeft: wp( '10%' ),
+  // },
+  // successModalWalletNameText: {
+  //   color: Colors.black,
+  //   fontSize: RFValue( 25 ),
+  //   fontFamily: Fonts.Regular,
+  //   textAlign: 'center',
+  //   paddingRight: 10,
+  //   flex: 1,
+  // },
+  // successModalAmountImage: {
+  //   width: wp( '15%' ),
+  //   height: wp( '15%' ),
+  //   marginRight: 15,
+  //   marginLeft: 10,
+  //   // marginBottom: wp("1%"),
+  //   resizeMode: 'contain',
+  // },
+  // successModalAmountText: {
+  //   color: Colors.black,
+  //   fontFamily: Fonts.Regular,
+  //   fontSize: RFValue( 21 ),
+  //   marginLeft: 5,
+  // },
+  // successModalAmountUnitText: {
+  //   color: Colors.borderColor,
+  //   fontFamily: Fonts.Regular,
+  //   fontSize: RFValue( 11 ),
+  // },
+  // successModalAmountInfoView: {
+  //   flex: 0.4,
+  //   marginRight: wp( '10%' ),
+  //   marginLeft: wp( '10%' ),
+  // },
   successModalButtonView: {
     height: wp( '13%' ),
     width: wp( '35%' ),
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 8,
-    elevation: 10,
-    shadowColor: Colors.shadowBlue,
-    shadowOpacity: 1,
-    shadowOffset: {
-      width: 15, height: 15
-    },
     backgroundColor: Colors.blue,
     alignSelf: 'center',
     marginLeft: wp( '8%' ),
@@ -223,48 +217,48 @@ const styles = StyleSheet.create( {
     fontSize: RFValue( 13 ),
     fontFamily: Fonts.Medium,
   },
-  separator: {
-    height: 2,
-    marginLeft: wp( '2%' ),
-    marginRight: wp( '2%' ),
-    backgroundColor: Colors.borderColor,
-  },
-  sendSuccessView: {
-    marginRight: wp( '8%' ),
-    marginLeft: wp( '8%' ),
-    marginBottom: wp( '1%' ),
-    flexDirection: 'row',
-  },
-  sendSuccessInfoTitle: {
-    color: Colors.textColorGrey,
-    fontFamily: Fonts.Regular,
-    fontSize: RFValue( 11 ),
-  },
-  contactProfileView: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: hp( '1.7%' ),
-  },
-  contactProfileImage: {
-    borderRadius: 60 / 2,
-    width: 60,
-    height: 60,
-    resizeMode: 'cover',
-    shadowColor: Colors.shadowBlue,
-    shadowOpacity: 1,
-    shadowOffset: {
-      width: 15, height: 15
-    },
-  },
-  contactNameText: {
-    color: Colors.textColorGrey,
-    fontSize: RFValue( 20 ),
-    fontFamily: Fonts.Regular,
-    marginLeft: 25,
-  },
-  contactIconImage: {
-    width: 20,
-    height: 20,
-    resizeMode: 'cover',
-  },
+  // separator: {
+  //   height: 2,
+  //   marginLeft: wp( '2%' ),
+  //   marginRight: wp( '2%' ),
+  //   backgroundColor: Colors.borderColor,
+  // },
+  // sendSuccessView: {
+  //   marginRight: wp( '8%' ),
+  //   marginLeft: wp( '8%' ),
+  //   marginBottom: wp( '1%' ),
+  //   flexDirection: 'row',
+  // },
+  // sendSuccessInfoTitle: {
+  //   color: Colors.textColorGrey,
+  //   fontFamily: Fonts.Regular,
+  //   fontSize: RFValue( 11 ),
+  // },
+  // contactProfileView: {
+  //   flexDirection: 'row',
+  //   alignItems: 'center',
+  //   marginTop: hp( '1.7%' ),
+  // },
+  // contactProfileImage: {
+  //   borderRadius: 60 / 2,
+  //   width: 60,
+  //   height: 60,
+  //   resizeMode: 'cover',
+  //   shadowColor: Colors.shadowBlue,
+  //   shadowOpacity: 1,
+  //   shadowOffset: {
+  //     width: 15, height: 15
+  //   },
+  // },
+  // contactNameText: {
+  //   color: Colors.textColorGrey,
+  //   fontSize: RFValue( 20 ),
+  //   fontFamily: Fonts.Regular,
+  //   marginLeft: 25,
+  // },
+  // contactIconImage: {
+  //   width: 20,
+  //   height: 20,
+  //   resizeMode: 'cover',
+  // },
 } )
