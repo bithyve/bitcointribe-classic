6d90c88ecbb62dbd833ecc2efcf8c9bb4867ccf1
akshay8551 <akshaychavan8551@gmail.com>
2024-05-20 14:42:47 +0530

    Removed unused import

--- src/pages/Accounts/Details/TransactionsPreviewSection.tsx
+++ src/pages/Accounts/Details/TransactionsPreviewSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import BitcoinUnit from '../../../common/data/enums/BitcoinUnit'
 import SubAccountKind from '../../../common/data/enums/SubAccountKind'
 import TransactionDescribing from '../../../common/data/models/Transactions/Interfaces'
